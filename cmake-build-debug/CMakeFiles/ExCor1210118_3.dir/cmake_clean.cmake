0fbda6a012f8fce9d1fc2d5cb8e966d41993177b
HanhND <hanhndth1808055@fpt.edu.vn>
2018-10-12 20:50:11 +0700

    Bai 3

---
+++ cmake-build-debug/CMakeFiles/ExCor1210118_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExCor1210118_3.dir/main.c.obj"
+  "ExCor1210118_3.pdb"
+  "ExCor1210118_3.exe"
+  "ExCor1210118_3.exe.manifest"
+  "libExCor1210118_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ExCor1210118_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
